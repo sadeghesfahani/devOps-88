de15fd5c3ecfc2e998f64133e49610a0ec83d0b5
nimabht <nimabeheshtaein99@gmail.com>
2023-05-19 14:22:51 +0330

    [fix] bug fixed.

--- product-dooni/src/user/user.controller.ts
+++ product-dooni/src/user/user.controller.ts
@@ -3,20 +3,15 @@ import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-
   constructor(private readonly productService: UserService) {}
 
   @Get('/update-user')
   updateUser() {
     return 'user updated successfully';
   }
-}
-
-
 
   @Get('/createUser')
   createUser() {
     return 'user created successfully';
   }
 }
-
